2e720f598d14d24f623e9b49026a0d598590705f
PetersenArne <arne.petersen@kendrion.com>
2023-11-22 08:16:30 +0100

    ...

--- main.cpp
+++ main.cpp
@@ -1,61 +1,48 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
+#include <QCoreApplication>
 #include <QApplication>
+#include <QTextStream>
 #include <QWebEngineView>
+
+class MyWebEngineView : public QWebEngineView
+{
+    Q_OBJECT
+
+public:
+    MyWebEngineView(const QString& url, int width, int height)
+        : url_(url), width_(width), height_(height)
+    {
+        connect(this, &QWebEngineView::loadFinished, this, &MyWebEngineView::onLoadFinished);
+    }
+
+    void startLoad()
+    {
+        setUrl(QUrl(url_));
+        resize(width_, height_);
+        show();
+    }
+
+private slots:
+    void onLoadFinished(bool success)
+    {
+        if (success) {
+            // Page loaded successfully
+            QTextStream(stdout) << "Page loaded successfully" << endl;
+        } else {
+            // Error loading the page
+            QTextStream(stdout) << "Error loading the page" << endl;
+        }
+    }
+
+private:
+    QString url_;
+    int width_;
+    int height_;
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
-    QWebEngineView view;
 
     if (app.arguments().size() < 4) {
         QTextStream(stderr) << "Usage: program <port> <width> <height>" << endl;
@@ -67,15 +54,13 @@ int main(int argc, char *argv[])
     int height = app.arguments().at(3).toInt();
 
     /* disable "right click" menu */
-    view.setContextMenuPolicy( Qt::NoContextMenu );
+    MyWebEngineView view("http://localhost:" + port, width, height);
+    view.setContextMenuPolicy(Qt::NoContextMenu);
 
+    QTextStream(stdout) << "Set url to: http://localhost:" + port << endl;
 
-    QString url = "http://localhost:" + port;
-    QTextStream(stdout) << "Set url to: " + url << endl;
-
-    view.setUrl(QUrl(url));
-    view.resize(width, height);
-    view.show();
+    view.startLoad();
 
     return app.exec();
 }
+
